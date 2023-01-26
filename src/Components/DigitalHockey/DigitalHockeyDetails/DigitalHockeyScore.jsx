d5ea3bf7d5e83d1c5674812644667314144e8348
Uceiifali <musiicxxx@gmail.com>
2023-01-26 15:18:13 +0200

    upcoming-1

---
+++ src/Components/DigitalHockey/DigitalHockeyDetails/DigitalHockeyScore.jsx
@@ -0,0 +1,39 @@
+import React, { useState } from 'react';
+import TeamOne from '../TeamOne/TeamOne';
+import TeamTwo from '../TeamTwo/TeamTwo';
+import Time from '../Time';
+import { AiOutlineArrowDown } from 'react-icons/ai';
+import DigitalHockeyDetailContainer from './DigitalHockeyDetailContainer';
+const DigitalHockeyScore = ({ TeamOneScore, TeamTwoScore, time, upComing }) => {
+  const [active, setActive] = useState(false);
+  return (
+    <>
+      <div className="w-full h-[101px]  rounded-full mt-12 border border-blue outline-8 outline outline-bgDark flex items-center justify-center gap-4 z-40 bg-white relative flex-col">
+        <div className="flex items-center justify-center w-full">
+          <div>
+            <TeamOne TeamOneScore={TeamOneScore} />
+          </div>
+          <div>
+            <Time time={time} upComing={upComing} />
+          </div>
+          <div>
+            <TeamTwo TeamTwoScore={TeamTwoScore} />
+          </div>
+        </div>
+        <button
+          className="absolute right-10 top-auto hidden sm:flex"
+          onClick={() => setActive(!active)}
+        >
+          <div className="flex items-center justify-center w-12 h-12 rounded-full border border-blue">
+            <AiOutlineArrowDown className="text-xl font-blue" />
+          </div>
+        </button>
+      </div>
+      <div hidden={active === false}>
+        <DigitalHockeyDetailContainer />
+      </div>
+    </>
+  );
+};
+
+export default DigitalHockeyScore;
